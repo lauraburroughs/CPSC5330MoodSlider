f15b5915416b8ffba2590e41b41409284c64dc69
Laura Burroughs <hellolauraburroughs@gmail.com>
2026-01-21 12:45:11 -0600

    Working on the slider

--- MoodSlider/ViewController.swift
+++ MoodSlider/ViewController.swift
@@ -9,11 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var moodSlider: UISlider!
+    
+    @IBOutlet weak var moodLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func moodLevel(_ sender: UISlider) {
+        
+        let value = sender.value
+        
+        if value <= 20 {
+            moodLabel.text = "Very Sad 😢"
+        }
+        
+        else if value <= 40 {
+            moodLabel.text = "Sad 🙁"
+        }
+        
+        else if value <= 60 {
+            moodLabel.text = "Neutral 😐"
+        }
+        
+        else if value <= 80 {
+            moodLabel.text = "Happy 🙂"
+        }
+        
+        else {
+            moodLabel.text = "Very Happy 😄"
+        }
+        
+    }
+            
+    
 }
 
